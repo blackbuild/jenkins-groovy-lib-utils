06f1fe246dfdeeddaeb8990ee51c089c38e41b3e
Stephan Pauxberger <stephan@blackbuild.com>
2023-02-12 20:49:10 +0100

    Plugin does not really need a dependency to ast-checker

--- jenkins-groovy-lib-plugin/src/main/java/com/blackbuild/groovycps/plugin/GroovyCpsPlugin.java
+++ jenkins-groovy-lib-plugin/src/main/java/com/blackbuild/groovycps/plugin/GroovyCpsPlugin.java
@@ -23,20 +23,21 @@
  */
 package com.blackbuild.groovycps.plugin;
 
-import com.blackbuild.groovy.cps.astchecker.AstChecker;
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
 import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.internal.provider.DefaultProvider;
 import org.gradle.api.plugins.GroovyPlugin;
 import org.gradle.api.provider.Provider;
 import org.gradle.api.resources.TextResource;
 import org.gradle.api.tasks.compile.GroovyCompile;
 
 import java.io.File;
-import java.net.URL;
-import java.security.CodeSource;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * Plugin that prepares a project for CPS usage. Applies the "groovy" plugin
@@ -74,21 +75,24 @@ public class GroovyCpsPlugin implements Plugin<Project> {
 
     private void addCpsDependenciesTo(Configuration groovy) {
         groovy.defaultDependencies(d -> {
-            d.add(project.getDependencies().create(project.files(getLocationForClass(AstChecker.class))));
+            d.add(createDependency("com.blackbuild.groovycps:ast-checker", new DefaultProvider<>(this::getOwnVersion)));
             d.add(createDependency("org.codehaus.groovy:groovy-all", extension.getGroovyVersion()));
             d.add(createDependency("com.cloudbees:groovy-cps", extension.getCpsVersion()));
         });
     }
 
-    private Dependency createDependency(String artifactAndGroup, Provider<String> version) {
-        return project.getDependencies().create(artifactAndGroup + ":" + version.get());
+    private String getOwnVersion() {
+        Properties props = new Properties();
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("com.blackbuild.jenkins.groovy-cps.properties")) {
+            props.load(is);
+        } catch (IOException e) {
+            throw new GradleException("Could not determine version of groovy cps plugin.",e);
+        }
+        return props.getProperty("version");
     }
 
-    private static URL getLocationForClass(Class<?> type) {
-        CodeSource source = type.getProtectionDomain().getCodeSource();
-        if (source == null)
-            throw new GradleException("Could not determine classpath of " + type);
-        return source.getLocation();
+    private Dependency createDependency(String artifactAndGroup, Provider<String> version) {
+        return project.getDependencies().create(artifactAndGroup + ":" + version.get());
     }
 
     private void activateCps() {
